9ec11a0da1d8489ada343d413cab3b04101ad90e
Vitaliy Inshakov <michael8913@gmail.com>
2022-12-22 15:04:33 +0200

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Виталий Иншаков on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
